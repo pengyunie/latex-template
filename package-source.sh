c2f0a15d4cbb1e53853891dbf7330743f37e16ed
Pengyu Nie <prodigy.sov@gmail.com>
2021-03-01 17:19:44 -0600

    Update script for packaging .

--- package-source.sh
+++ package-source.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+set -x
+set -e
+
+make clean && make
+rm -rf source.zip source
 mkdir source
 python3 format.py main.tex
 mv formatted.tex source/main.tex
-cp bib.bib ACM-Reference-Format.bst acmart.cls source/
+
+# ACM
+cp main.bbl ACM-Reference-Format.bst acmart.cls source/
+# IEEE
+#cp main.bbl IEEEtran.bst IEEEtran.cls source/
+
 cp -r figs source/
 cp -r tables source/
 

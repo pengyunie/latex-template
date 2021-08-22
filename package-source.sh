f562e04600442202a38bf1ac21f5351c0316be9b
Pengyu Nie <prodigy.sov@gmail.com>
2021-08-21 21:34:42 -0500

    Move defs* to defs/ directory; Fix some problem in package-source.sh.

--- package-source.sh
+++ package-source.sh
@@ -12,10 +12,15 @@ mv formatted.tex source/main.tex
 # ACM
 cp main.bbl ACM-Reference-Format.bst acmart.cls source/
 # IEEE
-#cp main.bbl IEEEtran.bst IEEEtran.cls source/
+# cp main.bbl bib.bib IEEEtran.bst IEEEtran.cls source/
 
+cp -r defs source/
 cp -r figs source/
 cp -r tables source/
 
 zip -r source source
 rm -rf source/
+
+set +x
+set +e
+echo "Did you remember to clean irrelevant files before packaging? You can (1) run `git clean . -xdi` (2) manually delete unused files in figs/ and tables/" >&2
